5b42f59b803a8202c238cb8b6852f5216bf96024
20161104616 <992846873@qq.com>
2018-11-23 15:58:07 +0800

    Initial Commit

---
+++ fir3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fir3
+//
+//  Created by 20161104616 on 2018/11/23.
+//  Copyright © 2018 20161104616. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ fir3Tests/fir3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  fir3Tests.swift
+//  fir3Tests
+//
+//  Created by 20161104616 on 2018/11/23.
+//  Copyright © 2018 20161104616. All rights reserved.
+//
+
+import XCTest
+@testable import fir3
+
+class fir3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
